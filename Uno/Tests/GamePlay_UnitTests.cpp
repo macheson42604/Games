eca993258ba0b31dd01feead1228be2e9d40d669
Marjorie Acheson <118392504+macheson42604@users.noreply.github.com>
2025-12-08 19:08:46 -0700

    Setting up Gameplay.cpp unit test file

---
+++ Uno/Tests/GamePlay_UnitTests.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "../GamePlay.hpp"
+
+using namespace std;
+
+int maint() {
+    cout << "----- Running Gameplay.cpp Tests -----" << endl;
+
+    cout << "----- All Tests Complete -----" << endl;
+
+    return 0;
+}
